771a4f37ada60cbe332bf6280d139a1473605fc6
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-06-08 14:14:01 -0700

    Printing January

--- Labs/Lab4.cpp
+++ Labs/Lab4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 bool is_leap(int year) {
     bool isLeap;
     if (year % 4 != 0)
@@ -12,13 +13,47 @@ bool is_leap(int year) {
         isLeap = true;
     return isLeap;
 }
-
+int number_of_days_in_a_month(int a_month) {
+    if (a_month == 1 || a_month == 3 || a_month == 5 || a_month == 7 || a_month == 8 || a_month == 10 || a_month == 12)
+        return 31;
+    else
+        return 30;
+}
 int main()
 {
     int year;
-    cout << "Enter the year for which you wish to generate the calendar: ";
-    cin >> year;
-    cout << is_leap(year);
+    int firstDay;
+    string weekdays[7] = { "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" };
+    const int WEEKS = 5;
+    const int DAYS_PER_WEEK = 7;
+
+    int arr[WEEKS][DAYS_PER_WEEK];
+    int day_of_month = 1;
+
+    //jan 2004 starting from sun
+    for (int i = 0; i < WEEKS; i++) {
+        for (int j = 0; j < DAYS_PER_WEEK; j++) {
+            if (day_of_month == number_of_days_in_a_month(1))
+                break;
+            arr[i][j] = day_of_month++;
+        }
+    }
+
+    for (int i = 0; i < WEEKS; i++) {
+        for (int j = 0; j < DAYS_PER_WEEK; j++) {
+            if (day_of_month == number_of_days_in_a_month(1))
+                break;
+            cout << arr[i][j] << " ";
+        }
+    }
+
+
+    //cout << "Enter the year for which you wish to generate the calendar: ";
+    //cin >> year;
+
+    //cout << endl << "Enter the day of the week that January first is on: ";
+    //cin >> firstDay;
+
     return 0;
 }
 
