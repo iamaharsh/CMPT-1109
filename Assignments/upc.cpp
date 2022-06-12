0a92e3d862847c735c3bf7be534e4631ff6e5a83
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-06-11 21:01:45 -0700

    Menu

---
+++ Assignments/upc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void printMenu() {
+	cout << "Check Digit Calculator\n--------------------\n1. EAN - 8 \n2. UPC - A\n3. EAN - 13\n4. EAN - 14\n5. SSCC\n-------------------- ";
+}
+int main() {
+	printMenu();
+
+	return 0;
+}
