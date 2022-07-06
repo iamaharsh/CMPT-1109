7875cd721759185596a08bf4741dbaf223d2f432
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-07-06 14:31:08 -0700

    Login Done

--- Labs/Lab6.cpp
+++ Labs/Lab6.cpp
@@ -2,31 +2,67 @@
 using namespace std;
 
 const int ROWS = 4;
-int COLUMNS_PER_ROW[ROWS] = { 5,4,6,3 };
+int COLUMNS_PER_ROW[ROWS] = { 5,6,4,3 };
+int** arr;
+void display();
 int main()
 {
-    int menuchoice;
-    cout << "1: Login User \n2: Logoff a user\n3: Search User";
-    cin >> menuchoice;
-
-    int** arr = new int* [ROWS];
+    arr = new int* [ROWS];
     arr[0] = new int[5];
     arr[1] = new int[6];
     arr[2] = new int[4];
     arr[3] = new int[3];
 
-    int user_id = 49193;
-    int machine = 2;
-    int lab = 3;
-
-    //simulate a login operation
-    arr[lab - 1][machine - 1] = user_id;
-
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLUMNS_PER_ROW[i]; j++) {
-            cout << arr[i][j] << " ";
-        }cout << endl;
+            arr[i][j] = 0;
+        }
     }
+
+
+    int menuchoice;
+    do {
+        
+        cout << "1: Login User \n2: Logoff a user\n3: Search User\n4: Exit" << endl;
+        cin >> menuchoice;
+        switch (menuchoice) {
+        case 1: {
+            int user_id = 49193;
+            int machine = 2;
+            int lab = 3;
+            cout << "enter id, lab & machine" << endl;
+            cin >> user_id >> lab >> machine;
+            arr[lab - 1][machine - 1] = user_id;
+            display();
+            break;
+        }
+              /*case 2: {
+
+              }
+              case 3: {
+
+              }*/
+        default:
+            break;
+        }
+    }while (menuchoice != 4);
+
+    delete [] arr[0];
+    delete [] arr[1];
+    delete [] arr[2];
+    delete [] arr[3];
+    delete[] arr;
+
     return 0;
 }
 
+void display() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS_PER_ROW[i]; j++) {
+            if (arr[i][j] == 0)
+                cout << "empty";
+            else
+                cout << arr[i][j] << " ";
+        }cout << endl;
+    }
+}
