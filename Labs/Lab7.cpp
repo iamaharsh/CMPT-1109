e2330b07c9bdd03fbbc37c4a25600d4970ea8ddb
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-07-13 13:11:27 -0700

    Added Objects and Classes

--- Labs/Lab7.cpp
+++ Labs/Lab7.cpp
@@ -1,32 +1,68 @@
 #include <iostream>
 using namespace std;
 class Movie {
+private:
     string name;
     string mpaaRating;
+    int terribleCount;
+    int badCount;
+    int okCount;
+    int goodCount;
+    int greatCount;
+public:
+    Movie(string name, string MPAA) {
 
+    }
+    Movie() {
+        
+    }
+    void addRating(int category) {
+        if (category == 1) {
+            terribleCount++;
+        }
+            
+    }
+    void setName(string a) {
+        name = a;
+    }
+    void setMPAA(string a) {
+        mpaaRating = a;
+    }
+    string getName() {
+        return name;
+    }
+    string getMPAA() {
+        return mpaaRating;
+    }
+    double getAverage() {
+
+    }
 };
+
+
 int main()
 {
     Movie movie1("300", "R");
-    movie1.addrating(1);
-    movie1.addrating(1);
-    movie1.addrating(3);
-    movie1.addrating(3);
-    movie1.addrating(5);
-    movie1.getName();
-    movie1.getMPAA();
+    movie1.addRating(1);
+    movie1.addRating(1);
+    movie1.addRating(3);
+    movie1.addRating(3);
+    movie1.addRating(5);
+    movie1.getName(); //300
+    movie1.getMPAA(); //R
     cout << movie1.getAverage(); //3.2
 
     Movie movie2;
-    movie2.setName("The GodFather");
+    movie2.setName("The GodFather"); 
     movie2.setMPAA("PG-13");
     movie2.addRating(1);
     movie2.addRating(3);
     movie2.addRating(5);
     movie2.addRating(5);
     movie2.addRating(5);
-    movie2.getName();
-    movie2.getMPAA();
+    movie2.getName(); //The Godfather
+    movie2.getMPAA(); //PG-13
     cout << movie1.getAverage(); //3.8
+    return 0;
 }
 
