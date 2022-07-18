7575a751eee7b725be703af8a3c8c72705e9191b
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-07-18 01:13:43 -0700

    Declared member functions

---
+++ Assignments/Assignment2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class rationalNumber {
+private:
+	int numerator;
+	int denominator;
+	int ratnumber;
+public:
+	rationalNumber(int nume, int denom) {
+		ratnumber = nume / denom;
+	}
+	rationalNumber(int num) {
+		ratnumber = num / 1;
+	}
+	float add() {
+
+	}
+	float substract() {
+
+	}
+	float multiply() {
+
+	}
+	float divide() {
+
+	}
+	bool less() {
+
+	}
+	float negative(float number) {
+		return -number;
+	}
+	void input() {
+
+	}
+	void output() {
+
+	}
+};
+
+int main() {
+
+	return 0;
+}
