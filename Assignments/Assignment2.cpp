1726b4327f96ee343c7a9cbc20b8111bb89a4639
iamaharsh <106277133+iamaharsh@users.noreply.github.com>
2022-07-20 12:41:03 -0700

    Decalred Setters and Getters and Inputs

--- Assignments/Assignment2.cpp
+++ Assignments/Assignment2.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
 class rationalNumber {
 private:
 	int numerator;
 	int denominator;
 	int ratnumber;
 public:
-	rationalNumber(int nume, int denom) {
-		ratnumber = nume / denom;
+	//Constructors
+	rationalNumber(int num, int denom) {
+		numerator = num;
+		denominator = denom;
 	}
 	rationalNumber(int num) {
-		ratnumber = num / 1;
+		numerator = num;
+		denominator = 1;
 	}
+
+	//Setter Getter
+	void rationalNumber setNum(int numerator)
+	{
+		this->numerator = numerator;
+	}
+
+	int rationalNumber getNum()
+	{
+		return numerator;
+	}
+
+	void rationalNumber setDenom(int denom)
+	{
+		this->denominator = denominator;
+	}
+
+	int rationalNumber getDenom()
+	{
+		return denominator;
+	}
+
+
 	float add() {
 
 	}
@@ -31,7 +59,7 @@ public:
 		return -number;
 	}
 	void input() {
-
+		
 	}
 	void output() {
 
@@ -39,6 +67,21 @@ public:
 };
 
 int main() {
+	int n1, d1, n2, d2;
 
+	cout << "Rational Number#1 :" << endl;
+	cout << "Enter Numerator :";
+	cin >> n1;
+	cout << "Enter Denominator :";
+	cin >> d1;
+
+	cout << " Rational Number#2 ::" << endl;
+	cout << "Enter Numerator :";
+	cin >> n2;
+	cout << "Enter Denominator :";
+	cin >> d2;
+
+	rationalNumber(n1, d1);
+	rationalNumber(n2, d2);
 	return 0;
 }
